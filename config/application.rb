c36e5ce264fa22de86454638f1811cedac125040
quotto <rhyme.and.rhyme@gmail.com>
2014-10-19 03:46:42 +0900

    modified: config font precompile

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,7 @@ module Sampler
     # Add the fonts path
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
