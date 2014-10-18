2aed8d935f691966324fe0dde06624e4a09deb2b
quotto <rhyme.and.rhyme@gmail.com>
2014-10-19 03:27:56 +0900

    modified: apply fonts asset

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module Sampler
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Add the fonts path
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
